06e235544b36bd80531ad343b15f8d838be66f88
Kannav02 <kannavsethi02@gmail.com>
2025-01-21 14:35:45 -0500

    feat: reverse_string added

---
+++ hashmap_and_arrays/reverse_string_chars.py
@@ -0,0 +1,14 @@
+def reverse_string(s:str)->str:
+    i = 0
+    j = len(s) - 1
+    s = list(s)
+    while i <= j:
+        if not s[i].isalpha():
+            i+=1
+        if not s[j].isalpha():
+            j-=1
+        else:
+            s[i],s[j] = s[j],s[i]
+            i+=1
+            j-=1
+    return "".join(s)
